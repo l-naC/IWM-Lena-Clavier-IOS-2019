302af7c0ed62dc7a4401bbafc4735287c7815e7c
jpo <jpo@IMAC-E309-17.local>
2019-11-18 16:54:21 +0100

    add Table View

--- IWM-Lena-Clavier-IOS-2019/HomeViewController.swift
+++ IWM-Lena-Clavier-IOS-2019/HomeViewController.swift
@@ -8,15 +8,30 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+  
     
+    var lignes = ["ligne 1","ligne 2","ligne 3", "ligne 4", "ligne 5", "ligne 6", "ligne 7", "ligne 8", "ligne 9", "ligne 10", "ligne 11", "ligne 12", "ligne 13", "ligne 14"]
+    
+    // Return the number of rows for the table.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return lignes.count
+    }
 
+    // Provide a cell object for each row.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       // Fetch a cell of the appropriate type.
+       let cell = tableView.dequeueReusableCell(withIdentifier: "cellTypeIdentifier", for: indexPath)
+
+        cell.textLabel?.text = lignes[indexPath.row]
+        return cell
+    }
     /*
     // MARK: - Navigation
 
